e32eacd8fa5605f9b7958e18a3ff638581c8681c
Olivia Jennings <oejennings@users.noreply.github.com>
2023-10-11 11:14:32 -0400

    added EventList component to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // src/App.js
 
+import EventList from './components/EventList';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <div id="event-list"></div>
+      <EventList />
     </div>
   );
 }
