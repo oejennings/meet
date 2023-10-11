80850ba968018ecc956c50213e1f517006ed6871
Olivia Jennings <oejennings@users.noreply.github.com>
2023-10-11 14:41:21 -0400

    added Number of Events test

---
+++ src/_tests_/NumberOfEvents.test.js
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('<NumberOfEvents /> Component', () => {
+    let NumberOfEventsComponent; 
+    beforeEach(() => {
+        NumberOfEventsComponent = render( <NumberofEvents  /> );
+    });
+    
+    test('render text input', () => {
+        const input = NumberOfEventsComponent.queryByRole('textbox');
+        expect(input).toBeInTheDocument();
+    });
+
+    test('render default number of event to 32', () => {
+        const input = NumberOfEventsComponent.queryByRole('textbox');
+        expect(input).toHaveValue('32');
+    });
+
+    test('updates number of event correctly when user types in textbox', () => {
+        const input = NumberOfEventsComponent.queryByRole('textbox');
+        await userEvent.type(input, '{backspace}{backspace}10');
+        expect(input).toHaveValue('10');
+    })
+})
