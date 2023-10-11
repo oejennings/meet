7e36313245393ad61949890b6fa8515cfb5d7120
Olivia Jennings <oejennings@users.noreply.github.com>
2023-10-11 11:50:33 -0400

    added CitySearch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // src/App.js
 
+import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
+      <CitySearch />
       <EventList />
     </div>
   );
