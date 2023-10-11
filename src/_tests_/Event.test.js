479f82fe3faf63d9b553d0aff4c949cc3b9a6b07
Olivia Jennings <oejennings@users.noreply.github.com>
2023-10-11 13:04:12 -0400

    created Event.test.js file

---
+++ src/_tests_/Event.test.js
@@ -0,0 +1,6 @@
+// src/_test_/Event.test.js
+
+import Event from '../components/Event';
+import { render } from '@testing-library/react';
+import mockData from '../mock-data';
+import userEvent from '@testing-library/user-event';
