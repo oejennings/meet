64d020e70a57fc92d91c2130675ced4b87d38b62
Olivia Jennings <oejennings@users.noreply.github.com>
2023-10-11 11:07:12 -0400

    created App.test.js file
    inside new _tests_ folder

---
+++ src/_tests_/App.test.js
@@ -0,0 +1,11 @@
+// src/_tests_/App.test.js
+
+import { render } from '@testing-library/react';
+import App from '../App';
+
+describe('<App /> component', () => {
+    test('renders list of events', () => {
+        const AppDOM = render(<App />).container.firstChild;
+        expect(AppDOM.querySelector('#event-list')).toBeInTheDocument(); 
+    });
+});
