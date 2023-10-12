3aa264d9c9637570ba679fd668ff2ae3d3c046be
Olivia Jennings <oejennings@users.noreply.github.com>
2023-10-12 12:53:43 -0400

    updated NumberOfEvents unit tests

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
-// import NumberOfEvents from './components/NumberOfEvents';
+import NumberOfEvents from './components/NumberOfEvents';
 import { extractLocations, getEvents } from './api'
 
 import './App.css';
@@ -12,21 +12,25 @@ const App = () => {
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
 
-   useEffect(() => {
+  useEffect(() => {
     fetchData();
-  }, []);
+  }, [currentCity]);
 
   const fetchData = async () => {
     const allEvents = await getEvents();
-    setEvents(allEvents.slice(0, currentNOE));
+    const filteredEvents = currentCity === "See all cities" ?
+      allEvents :
+      allEvents.filter(event => event.location === currentCity)
+    setEvents(filteredEvents.slice(0, currentNOE));
     setAllLocations(extractLocations(allEvents));
   }
 
   return (
     <div className="App">
-      <CitySearch allLocations={allLocations}/>
-      {/* <NumberOfEvents /> */}
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity}/>
+      <NumberOfEvents /> 
       <EventList events={events} />
     </div>
   );
--- src/_tests_/NumberOfEvents.test.js
+++ src/_tests_/NumberOfEvents.test.js
@@ -8,19 +8,23 @@ describe('<NumberOfEvents /> Component', () => {
         NumberOfEventsComponent = render( <NumberOfEvents /> );
     });
     
-    test('render text input', () => {
-        const input = NumberOfEventsComponent.queryByRole('textbox');
-        expect(input).toBeInTheDocument();
+    test('renders number of events text input', () => {
+        const numberTextBox = NumberOfEventsComponent.queryByRole('textbox');
+        expect(numberTextBox).toBeInTheDocument();
+        expect(numberTextBox).toHaveClass('number-of-events-input');
     });
 
-    test('render default number of event to 32', () => {
-        const input = NumberOfEventsComponent.queryByRole('textbox');
-        expect(input).toHaveValue('32');
+    test('default number is 32', async () => {
+        const numberTextBox = NumberOfEventsComponent.queryByRole('textbox');
+        expect(numberTextBox).toHaveValue("32");
     });
 
-    test('updates number of event correctly when user types in textbox', async () => {
-        const input = NumberOfEventsComponent.queryByRole('textbox');
-        await userEvent.type(input, '{backspace}{backspace}10');
-        expect(input).toHaveValue('10');
-    })
+    test('number of events text box value changes when the user types in it', async () => {
+        const user = userEvent.setup();
+        const numberTextBox = NumberOfEventsComponent.queryByRole('textbox');
+        await user.type(numberTextBox, "123")
+    
+        // 32 (the default value already written) + 123
+        expect(numberTextBox).toHaveValue("32123");
+    });
 })
