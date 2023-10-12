562c254ac3690ca98385773211b984e7e4de11d9
Olivia Jennings <oejennings@users.noreply.github.com>
2023-10-12 11:33:39 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 // src/App.js
-
+import { useEffect, useState } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import NumberOfEvents from './components/NumberOfEvents';
+import { getEvents } from './api'
+
 import './App.css';
 
+
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <CitySearch />
       <NumberOfEvents />
-      <EventList />
+      <EventList events={events} />
     </div>
   );
 }
