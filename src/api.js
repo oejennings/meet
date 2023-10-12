41705acefd13707db9c9ad2feb2e592625aec5a6
Olivia Jennings <oejennings@users.noreply.github.com>
2023-10-12 13:15:20 -0400

    check if token is valid

--- src/api.js
+++ src/api.js
@@ -16,12 +16,46 @@ export const extractLocations = (events) => {
   return locations;
 };
 
-/**
- *
- * This function will fetch the list of all events
- */
+const checkToken = async (accessToken) => {
+  const response = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  );
+  const result = await response.json();
+  return result;
+};
+
+const removeQuery = () => {
+  let newurl;
+  if (window.history.pushState && window.location.pathname) {
+    newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
+
+// This function will fetch the list of all events
 export const getEvents = async () => {
-  return mockData;
+  if (window.location.href.startsWith('http://localhost')) {
+    return mockData;
+  }
+
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url =  "https://9xtzmebnpd.execute-api.us-east-1.amazonaws.com/dev/api/get-events" + "/" + token;
+    const response = await fetch(url);
+    const result = await response.json();
+    if (result) {
+      return result.events;
+    } else return null; 
+  }
 };
 
 export const getAccessToken = async () => {
