a1bc79b59fd6ac6d329f6955a571422e226d99f5
Olivia Jennings <oejennings@users.noreply.github.com>
2023-10-18 11:52:30 -0400

    used beforeAll and afterAll functions
    for End to End test

--- src/_tests_/EndToEnd.test.js
+++ src/_tests_/EndToEnd.test.js
@@ -1,30 +1,27 @@
 import puppeteer from "puppeteer";
 
 describe('show/hide an event details', () => {
+    let browser;
+    let page;
+    beforeAll(async () => {
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+        await page.waitForSelector('.event');
+    });
+
+    afterAll(() => {
+        browser.close();
+    });
 
     test('An event element is collapsed by default', async () => {
-        const browser = await puppeteer.launch();
-    
-        const page = await browser.newPage();
-        await page.goto('http://localhost:3000/');
-    
-        await page.waitForSelector('.event');
-    
         const eventDetails = await page.$('.event .details');
         expect(eventDetails).toBeNull();
-        browser.close();
       });
 
       test('User can expand an event to see its details', async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('http://localhost:3000/');
-    
-        await page.waitForSelector('.event');
         await page.click('.event .details-btn');
-    
         const eventDetails = await page.$('.event .details');
         expect(eventDetails).toBeDefined();
-        browser.close();
       });
 });
