bef4dcd65be6e17222b3282fd3123af19ec5f321
Olivia Jennings <oejennings@users.noreply.github.com>
2023-10-24 12:56:23 -0400

    added error alert to app component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { extractLocations, getEvents } from './api'
-import { InfoAlert } from './components/Alert';
+import { InfoAlert, ErrorAlert } from './components/Alert';
 
 import './App.css';
 
@@ -15,6 +15,7 @@ const App = () => {
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -33,12 +34,15 @@ const App = () => {
     <div className="App">
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert}/> :null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert}/> :null}
       </div>
       <CitySearch 
         allLocations={allLocations} 
         setCurrentCity={setCurrentCity}
         setInfoAlert={setInfoAlert} />
-      <NumberOfEvents setCurrentNOE={setCurrentNOE} /> 
+      <NumberOfEvents 
+        setCurrentNOE={setCurrentNOE} 
+        setErrorAlert={setErrorAlert}/> 
       <EventList events={events} />
     </div>
   );
