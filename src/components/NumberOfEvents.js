7b1c7282d653012e759dda9b63c019c62789f031
Olivia Jennings <oejennings@users.noreply.github.com>
2023-10-24 13:01:28 -0400

    added errorAlert to NumberOfEvents

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,10 +1,20 @@
 // src/components/NumberOfEvents.js
 
-const NumberOfEvents = ({ setCurrentNOE }) => {
+const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
 
   const handleInputChanged = (event) => {
     const value = event.target.value;
     setCurrentNOE(value);
+
+    let infoText;
+    if (isNaN(value) || value <= 0) {
+      infoText = "Please enter a number greater than 0.";
+      setErrorAlert(infoText);
+    } else {
+      infoText = "";
+      setErrorAlert(infoText);
+      setCurrentNOE(value);
+    }
   }
 
   return (
