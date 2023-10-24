c7ddc271e8bce0a649634093a0911062e32fb352
Olivia Jennings <oejennings@users.noreply.github.com>
2023-10-24 12:42:50 -0400

    updated citysearch tests to pass
    with setInfoAlert

--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -9,7 +9,10 @@ import App from '../App';
 describe('<CitySearch /> component', () => {
     let CitySearchComponent;
   beforeEach(() => {
-    CitySearchComponent = render(<CitySearch allLocations={[]}/>);
+    CitySearchComponent = render(<CitySearch 
+      allLocations={[]}
+      setCurrentCity={() => { }}
+      setInfoAlert={() => { }}/>);
   });
 
   test('renders text input', () => {
@@ -36,7 +39,7 @@ describe('<CitySearch /> component', () => {
     const user = userEvent.setup();
     const allEvents = await getEvents();
     const allLocations = extractLocations(allEvents);
-    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} setInfoAlert={() => { }} />);
 
     // user types "Berlin" in city textbox
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
@@ -61,7 +64,8 @@ describe('<CitySearch /> component', () => {
     const allLocations = extractLocations(allEvents);
     CitySearchComponent.rerender(<CitySearch 
       allLocations={allLocations} 
-      setCurrentCity={() => { }} 
+      setCurrentCity={() => { }}
+      setInfoAlert={() => { }}
     />);
 
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
