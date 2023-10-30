cf45a58cf9d3f6a182246ff8e41924dd6514d8d3
Olivia Jennings <oejennings@users.noreply.github.com>
2023-10-30 13:14:36 -0400

    filled out chart component

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -8,8 +8,39 @@ import {
   Tooltip
 } from 'recharts'
 
-const EventGenresChart = () => {
+const EventGenresChart = ({ events }) => {
+    const [data, setData] = useState([]);
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
 
-}
+    useEffect(() => {
+        setData(getData());
+    }, [`${events}`]);
+
+    const getData = () => {
+        const data = genres.map((genre) => {
+            const filteredEvents = events.filter((event) => event.summary.includes(genre));
+            return {
+                name: genre, 
+                value: filteredEvents.length
+            };
+        });
+        return data;
+    };
+
+    return (
+        <ResponsiveContainer width="99%" height={400}>
+          <PieChart>
+            <Pie
+              data={data}
+              dataKey="value"
+              fill="#8884d8"
+              labelLine={false}
+              label
+              outerRadius={130}           
+            />
+          </PieChart>
+        </ResponsiveContainer>
+      );
+};
 
 export default EventGenresChart;
