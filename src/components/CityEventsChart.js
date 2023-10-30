125d0d6245dfe3b0a1cdffaedee7e975a08e1890
Olivia Jennings <oejennings@users.noreply.github.com>
2023-10-30 12:41:52 -0400

    updated margin

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -33,7 +33,7 @@ import {
               top: 20,
               right: 20,
               bottom: 60,
-              left: 20,
+              left: -30,
             }}
           >
             <CartesianGrid />
